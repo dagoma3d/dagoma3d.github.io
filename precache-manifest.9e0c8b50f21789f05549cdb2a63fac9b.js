36a9a851b97d4fe2519b5db5622f7826e16df4e3
orel <orel@dagoma.fr>
2020-10-07 16:11:15 +0200

    Initial commit

---
+++ precache-manifest.9e0c8b50f21789f05549cdb2a63fac9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23c4038e44bab18b78c69f48697bb01b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11a5f776a8f6db080e0",
+    "url": "/static/css/main.c0224ec5.chunk.css"
+  },
+  {
+    "revision": "bf5c3e21ff33e57211c0",
+    "url": "/static/js/2.931ffefe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.931ffefe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a11a5f776a8f6db080e0",
+    "url": "/static/js/main.f1dfcc92.chunk.js"
+  },
+  {
+    "revision": "e39cdea50821df4ae83d",
+    "url": "/static/js/runtime-main.c577a285.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  }
+]);
